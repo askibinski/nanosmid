9e50e3303e281fdd37921b7fd681b7732a127c87
Albert Skibinski <albert.skibinski@gmail.com>
2017-02-01 22:44:31 +0100

    json api call

--- web/scripts/scripts.js
+++ web/scripts/scripts.js
@@ -5,9 +5,25 @@
 class drupalConnector {
 
 	constructor() {
-
+		this.hello();
+		this.node1 = this.get('https://service.nanosmid.nl/node/1?_format=api_json');
+		console.log(JSON.parse(this.node1));
 	}
+
+	hello() {
+		console.log('hi!');
+	}
+
+	get(url) {
+    	var Httpreq = new XMLHttpRequest(); 
+    	Httpreq.open("GET",url,false);
+    	Httpreq.send(null);
+    	return Httpreq.responseText;          
+	}
+
+	// TODO: ombouwen naar async met promise:
+	https://www.tomas-dvorak.cz/posts/nodejs-request-without-dependencies/
 
 }
 
-var drupalConnection = new drupalConnector();
+var api = new drupalConnector();
