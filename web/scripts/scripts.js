b74f58749ab186749a64c3f63bb176a3acae5460
Albert Skibinski <albert.skibinski@gmail.com>
2017-02-15 21:09:15 +0100

    ES6 fetch added + output node 1

--- web/scripts/scripts.js
+++ web/scripts/scripts.js
@@ -1,28 +1,40 @@
 
+"use strict";
+
 /**
  * ES6 Class.
  */
 class drupalConnector {
 
 	constructor() {
-		this.hello();
-		this.node1 = this.get('https://service.nanosmid.nl/node/1?_format=api_json');
-		console.log(JSON.parse(this.node1));
+		this.node = this.get('https://service.nanosmid.nl/node/1?_format=api_json');
 	}
 
-	hello() {
-		console.log('hi!');
-	}
-
-	get(url) {
+	// This is the old method using an old fashioned xmlhttprequest.
+	oldGet(url) {
     	var Httpreq = new XMLHttpRequest(); 
     	Httpreq.open("GET",url,false);
     	Httpreq.send(null);
     	return Httpreq.responseText;          
 	}
 
-	// TODO: ombouwen naar async met promise:
-	// https://www.tomas-dvorak.cz/posts/nodejs-request-without-dependencies/
+	// https://jakearchibald.com/2015/thats-so-fetch
+	get(url) {
+		// This could be further enhanced using ES7 await.
+		fetch(url).then(function(response) {
+		  return response.json();
+		}).then(function(data) {
+			console.log('Connected to Drupal!');
+			var h1 = document.createElement("h1")
+			var title = document.createTextNode(data.attributes.title);
+			var body = document.createTextNode(data.attributes.body.value);
+			h1.appendChild(title);
+			document.getElementById('content').appendChild(h1);
+			document.getElementById('content').appendChild(body);
+		}).catch(function() {
+		  console.log("Booo");
+		});
+	}
 
 }
 
