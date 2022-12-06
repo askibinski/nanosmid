e9c36a9d6dd5629149a7188fab4b8b7378ce463e
askibinski <albert.skibinski@gmail.com>
2022-12-06 22:08:48 +0100

    Typescript interfaces

--- src/services/drupal-data.ts
+++ src/services/drupal-data.ts
@@ -1,57 +1,103 @@
-export interface Article {
-  title: string;
-  thumbnail: string;
-  categories: string[];
-  author: string;
-  pubDate: string;
-  userLink: string;
-  link: string;
+export interface DrupalNode extends JsonApiResourceWithPath {
+  drupal_internal__nid: number
+  drupal_internal__vid: number
+  changed: string
+  created: string
+  title: string
+  default_langcode: boolean
+  sticky: boolean
 }
 
-export interface Feed {
-  title: string;
-  image: string;
-  link: string;
+export interface JsonApiResourceWithPath extends JsonApiResource {
+  path: PathAlias
+}
+
+export type PathAlias = {
+  alias: string
+  pid: number
+  langcode: string
+}
+
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+export interface JsonApiResource extends Record<string, any> {
+  id: string
+  type: string
+  langcode: string
+  status: boolean
+}
+
+export interface Article extends DrupalNode {
+  field_article_type: string
 }
 
 export interface DrupalData {
   articles: Article[];
-  feed: Feed;
 }
 
-export interface RssFeedResult {
-  items: Article[];
-  status: string;
-  feed: Feed;
+export interface JsonApiResponse extends Record<string, any> {
+  jsonapi?: {
+    version: string
+    meta: Record<string, any>[]
+  }
+  data: Record<string, any>[]
+  errors: JsonApiError[]
+  meta: {
+    count: number
+    [key: string]: any
+  }
+  links?: JsonApiLinks
+  included?: Record<string, any>[]
 }
 
-async function fetchDrupalData(article_type: string): Promise<RssFeedResult> {
+// https://jsonapi.org/format/#error-objects
+export interface JsonApiError {
+  id?: string
+  status?: string
+  code?: string
+  title?: string
+  detail?: string
+  links?: JsonApiLinks
+}
+
+// https://jsonapi.org/format/#document-links
+export interface JsonApiLinks {
+  [key: string]: string | Record<string, string>
+}
+
+async function fetchDrupalData(article_type: string): Promise<JsonApiResponse> {
   let url = `https://albert.ddev.site/jsonapi/node/article?filter[field_article_type]=${article_type}`;
+  console.log(url);
   return (
     await fetch(url)
   ).json();
 }
 
-export async function getDrupalData(
-  article_type = "art",
-): Promise<DrupalData> {
-  const { feed, items, status } = await fetchDrupalData(article_type);
+export async function getDrupalData(article_type = "art"): Promise<DrupalData> {
+  const data = await fetchDrupalData(article_type);
 
-  console.log(feed);
-  console.log(items);
-  console.log(status);
-  const articles = items
-    .filter((item) => item.thumbnail)
-    .map((item) => {
-      return {
-        ...item,
-        userLink: feed.link,
-      };
-    });
+  console.log(data);
+  // Verify that the data is valid json data from Drupal.
+  if (!data.jsonapi) {
+    throw new Error("No data.");
 
-  if (!feed || status !== "ok") {
-    throw new Error("An error occured.");
   }
+  // console.log(items);
+  // console.log(status);
 
-  return { feed, articles };
+  // const articles = items
+  //   .filter((item) => item.thumbnail)
+  //   .map((item) => {
+  //     return {
+  //       ...item,
+  //       userLink: feed.link,
+  //     };
+  //   });
+  //
+  // if (!feed || status !== "ok") {
+  //   throw new Error("An error occured.");
+  // }
+
+  const articles: never[] = [];
+
+  return { articles };
 }
